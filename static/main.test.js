ef0faa64a9a6da7f3be6c90efc55c182a24f1681
edy-m <95467047+edy-m@users.noreply.github.com>
2025-05-01 16:01:44 -0700

    changed the index html file so that the api key isnt exposed and we need to extract from backend and append to url resulting in a nested fetch call for each of the six articles, also created a maintest js file that includes unit testing for the correct query, making sure api key is extracted, and today's date is being displayed

--- static/main.test.js
+++ static/main.test.js
@@ -1,3 +1,31 @@
+const fetch = require('node-fetch');
+
 test('basic sanity check', () => {
     expect(1 + 1).toBe(2);
+});
+
+test('making sure flask server returns API key as expected', async () => {
+    const response = await fetch('http://localhost:8000/api/key');
+    const data = await response.json();
+    console.log('Keyyyy:', data.apiKey);
+    expect(data.apiKey).toBe('LwRGjUYN8Phbf9LXDJZmzm7Ab99CQmke');
+});
+
+test('check that the query is correct (Sacramento)', async () => {
+    const response = await fetch('http://localhost:8000/api/key');
+    const data = await response.json();
+    url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=sacramento&api-key=' + data.apiKey;
+    console.log('queryyyy:', url);
+    expect(url).toBe('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=sacramento&api-key=LwRGjUYN8Phbf9LXDJZmzm7Ab99CQmke');
+});
+
+test('date at the top of the page is todays date', () => {
+    document.body.innerHTML = `<div id="date">
+        </div>`;
+    var now = new Date();
+    var date = now.toLocaleString();
+    document.getElementById("date").innerHTML = date;
+    var mockDate = document.getElementById("date");
+    console.log(mockDate.innerHTML);
+    expect(mockDate.innerHTML).toBe(date);
 });
